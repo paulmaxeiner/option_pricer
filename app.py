bac681393ccec9a4d2af083bbfa772f913dcbd60
paulmaxeiner <maxeiner@wustl.edu>
2025-07-13 18:06:48 -0500

    Added streamlit chart tabs

--- app.py
+++ app.py
@@ -11,9 +11,7 @@ model = st.selectbox("Model", ["Black-Scholes"])
 options = ["call","put"]
 option_type = st.radio("Option Type", options, index=0)
 
-
 S = st.number_input("Current Stock Price (S)", value=100.0, min_value=0.0, step=1.0)
-
 K = st.slider("Strike Price (K)", 1, 500, int(S), step=1)
 T = st.slider("Time to Maturity (T, years)", value=1.0, min_value=0.01, max_value=10.0, step=0.01)
 r = st.slider("Risk-Free Rate (r)", 0.0, 0.2, 0.05)
@@ -22,4 +20,30 @@ sigma = st.slider("Volatility (σ)", 0.01, 2.0, 0.2)
 
 
 price = black_scholes_price(S, K, T, r, q, sigma, option_type)
+
 st.subheader(f"Option Price: ${price:.2f}")
+
+stock_range = np.linspace(S*0.5, S*2, 100)
+option_values = [
+    black_scholes_price(s, K, T, r, q, sigma, option_type)
+    for s in stock_range
+]
+payoff_at_expiration = np.maximum(stock_range - K, 0) if option_type == 'call' else np.maximum(K - stock_range, 0)
+
+simulation_data = pd.DataFrame(
+    {
+        "Stock Price": stock_range,
+        "Option Price": option_values,
+        "Payoff at Expiration": payoff_at_expiration
+    }
+)
+
+st.header("Graphs")
+
+tab1, tab2 = st.tabs(["Payoff at Expiration", "Option Price vs Stock Price"])
+
+with tab1:
+    st.line_chart(simulation_data, x="Stock Price", y="Payoff at Expiration")
+with tab2:
+    st.line_chart(simulation_data, x="Stock Price", y="Option Price")
+
